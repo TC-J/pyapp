634980ba84d3204251e82a2d711a1192fe2e297c
TC Jowers <jowerstc@outlook.com>
2026-09-21 19:58:23 -0400

    finished yaml serde and load_package_file.

--- src/pyapp/__init__.py
+++ src/pyapp/__init__.py
@@ -1,2 +1,36 @@
-def main() -> None:
-    print("Hello from pyapp!")
+from pathlib import Path
+from typing import Any, Dict
+
+import yaml
+from importlib import resources
+
+
+def load_package_file(
+        module: str, 
+        relpath: str,
+) -> str:
+    """Load package-file on target at runtime.
+
+    Load package resource files using module-path and the relative-path to file. Must configure
+    pyproject.toml to package this resource-module.
+    """
+    return (resources.files(module) / relpath).read_text("utf-8")
+
+
+def extract_from_yaml(utf8_data: str, safe: bool) -> Dict[str, str]:
+    """Extract YAML from file into a dictionary.
+    """
+    return yaml.safe_load(utf8_data) if safe else yaml.load(utf8_data)
+
+
+def persist_to_yaml(data: dict, filepath: str | Path):
+    """Persist dictionary to file in YAML format."""
+    path: Path = Path(filepath)
+
+    if not path.exists():
+        path.touch()
+
+    with path.open("w+") as file:
+        file.write(yaml.dump(data))
+
+
